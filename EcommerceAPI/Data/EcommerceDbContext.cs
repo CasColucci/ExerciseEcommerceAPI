bf24dc4eb54fba1639f68b7798ca140cd9033325
Cas <51390039+CasColucci@users.noreply.github.com>
2024-09-18 17:08:29 -0400

    Added a seed to create default data

--- EcommerceAPI/Data/EcommerceDbContext.cs
+++ EcommerceAPI/Data/EcommerceDbContext.cs
@@ -11,5 +11,14 @@ namespace EcommerceAPI.Data
         }
 
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product { Id = 1, Name = "Laptop", Description = "A portable computer with a closing lid.", Price = 999, Category = Category.Electronics, CreatedDate = DateTime.Now}
+                );
+        }
     }
 }
---
+++ EcommerceAPI/Migrations/20240918210757_AddSeed.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EcommerceAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Category", "CreatedDate", "Description", "Name", "Price", "UpdatedDate" },
+                values: new object[] { 1, 1, new DateTime(2024, 9, 18, 17, 7, 57, 589, DateTimeKind.Local).AddTicks(8655), "A portable computer with a closing lid.", "Laptop", 999m, null });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
