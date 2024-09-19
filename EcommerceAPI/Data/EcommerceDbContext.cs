076f2cc69928fca91fdeee304fd7019ba5b52d6d
Cas <51390039+CasColucci@users.noreply.github.com>
2024-09-19 08:37:54 -0400

    Adding another seed

--- EcommerceAPI/Data/EcommerceDbContext.cs
+++ EcommerceAPI/Data/EcommerceDbContext.cs
@@ -17,7 +17,10 @@ namespace EcommerceAPI.Data
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.Entity<Product>().HasData(
-                new Product { Id = 1, Name = "Laptop", Description = "A portable computer with a closing lid.", Price = 999, Category = Category.Electronics, CreatedDate = DateTime.Now}
+                new Product { Id = 1, Name = "Laptop", Description = "A portable computer with a closing lid.", Price = 999, Category = Category.Electronics, CreatedDate = DateTime.Now},
+                new Product { Id = 2, Name = "Band T-Shirt", Description = "A t-shirt with the logo of a popular band.", Price = 20, Category = Category.Clothing, CreatedDate = DateTime.Today.AddDays(-2)},
+                new Product { Id = 3, Name = "Blender", Description = "A kitchen tool used to emulsify food.", Price = 35, Category = Category.Kitchen, CreatedDate = DateTime.Today.AddDays(-3) },
+                new Product { Id = 4, Name = "Lipstick", Description = "A tube of lipstick in a bright red color.", Price = 15, Category = Category.Beauty, CreatedDate = DateTime.Today.AddDays(-4) }
                 );
         }
     }
---
+++ EcommerceAPI/Migrations/20240919123540_AddingSeedItems.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EcommerceAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingSeedItems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2024, 9, 19, 8, 35, 40, 606, DateTimeKind.Local).AddTicks(3518));
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Category", "CreatedDate", "Description", "Name", "Price", "UpdatedDate" },
+                values: new object[,]
+                {
+                    { 2, 2, new DateTime(2024, 9, 17, 0, 0, 0, 0, DateTimeKind.Local), "A t-shirt with the logo of a popular band.", "Band T-Shirt", 20m, null },
+                    { 3, 4, new DateTime(2024, 9, 16, 0, 0, 0, 0, DateTimeKind.Local), "A kitchen tool used to emulsify food.", "Blender", 35m, null },
+                    { 4, 5, new DateTime(2024, 9, 15, 0, 0, 0, 0, DateTimeKind.Local), "A tube of lipstick in a bright red color.", "Lipstick", 15m, null }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2024, 9, 18, 17, 7, 57, 589, DateTimeKind.Local).AddTicks(8655));
+        }
+    }
+}
